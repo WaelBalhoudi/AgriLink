52d50d6e720743d04f705ad3331e0cf5bf79dbeb
Wael Balhoudi <147642888+WaelBalhoudi@users.noreply.github.com>
2026-03-11 08:06:43 +0100

    test

--- frontend/src/app/components/user/pages/home/home.component.ts
+++ frontend/src/app/components/user/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+ @ViewChild('fileInput') fileInput!: ElementRef;
 
+  openCamera() {
+    this.fileInput.nativeElement.click();
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+
+    if (file) {
+      console.log("Image selected:", file);
+
+      // Later you will send this image to backend
+      // this.uploadImage(file);
+    }
+  }
 }
